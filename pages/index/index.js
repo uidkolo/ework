27f15431c1446248840a32c4080e865f425a7841
huyang <745389963@qq.com>
2019-01-21 22:20:45 +0800

    home

---
+++ pages/index/index.js
@@ -0,0 +1,91 @@
+// pages/insue/issue.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list:[
+      {
+        id:'01',
+        name: '淘兼职',
+        description: '随时随地淘兼职',
+        cover: '/images/test/01.jpg'
+      },
+      {
+        id: '01',
+        name: '网络兼职',
+        description: '网络兼职，保证靠谱',
+        cover: '/images/test/02.jpg'
+      },
+      {
+        id: '01',
+        name: '兼职达人',
+        description: '用心为您服务每一份兼职',
+        cover: '/images/test/03.jpg'
+      },
+      {
+        id: '01',
+        name: '发传单',
+        description: '每日固定时间再学校门口发传单',
+        cover: '/images/test/04.jpg'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+  
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
